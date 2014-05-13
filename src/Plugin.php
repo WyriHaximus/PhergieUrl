3f71243db53fcacc6c1ab03bd4ac49c8b252d9e4
Cees-Jan <ceesjank@gmail.com>
2014-05-13 22:19:53 +0200

    Bare plugin for testing

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,6 +13,7 @@ namespace WyriHaximus\Phergie\Plugin\Url;
 use Phergie\Irc\Bot\React\AbstractPlugin;
 use Phergie\Irc\Bot\React\EventQueueInterface;
 use Phergie\Irc\Event\EventInterface;
+use React\Promise\Deferred;
 
 /**
  * Plugin for Display URL information about links.
@@ -31,7 +32,7 @@ class Plugin extends AbstractPlugin
      *
      * @param array $config
      */
-    public function __construct(array $config)
+    public function __construct(array $config = array())
     {
 
     }
@@ -44,9 +45,57 @@ class Plugin extends AbstractPlugin
     public function getSubscribedEvents()
     {
         return array(
-
+            'irc.received.privmsg' => 'handleIrcReceived',
         );
     }
 
+    public function handleIrcReceived(\Phergie\Irc\Event\UserEvent $event, \Phergie\Irc\Bot\React\EventQueue $queue)
+    {
+        $params = $event->getParams();
+        $extractor = new \Twitter_Extractor($params['text']);
+        $urls = $extractor->extractURLs();
+        array_walk($urls, array($this, 'handleUrl'));
+    }
+
+    public function handleUrl($url) {
+        $parsedUrl = parse_url($url);
+
+        if (!isset($parsedUrl['host']) && !isset($parsedUrl['path'])) {
+            return;
+        }
+
+        echo $url;
+        if ($this->emitUrlEvents($url)) {
+            $this->emitter->emit('http.request', array(new \WyriHaximus\Phergie\Plugin\Http\Request(array(
+                'url' => $url,
+                'responseCallback' => function($headers, $code) {
+                    var_export([$headers, $code]);
+                },
+                'resolveCallback' => function($data, $headers, $code) {
+                    var_export([$data, $headers, $code]);
+                },
+            ))));
+        }
+    }
+
+    protected function emitUrlEvents($url) {
+        $parsedUrl = parse_url($url);
+
+        $host = $parsedUrl['host'];
+        if (count($parsedUrl) == 1 && isset($parsedUrl['path'])) {
+            $host = $parsedUrl['path'];
+        }
+
+        if (substr($host, 0, 4) == 'www.') {
+            $host = substr($host, 4);
+        }
+
+        if (count($this->emitter->listeners('url.host.' . $host)) > 0) {
+            $this->emitter->emit('url.host.' . $host, array($url));
+            return false;
+        } else {
+            return true;
+        }
+    }
     
 }
