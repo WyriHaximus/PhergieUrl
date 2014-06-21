8d8c9209a419f384e4a3c5702854757bec4f14cd
Cees-Jan <ceesjank@gmail.com>
2014-06-21 17:47:05 +0200

    Url interface

---
+++ src/UrlInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of PhergieUrl.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WyriHaximus\Phergie\Plugin\Url;
+
+/**
+ * Interface UrlInterface
+ *
+ * @package WyriHaximus\Phergie\Plugin\Url
+ */
+interface UrlInterface
+{
+    public function getHeaders();
+    public function getCode();
+    public function getBody();
+    public function getUrl();
+}
