f26ec9a01743fce9243b77444a43f15e36743352
Cees-Jan <ceesjank@gmail.com>
2014-07-07 19:02:15 +0200

    Event object that enables url shortning, have to work some ideas out before we can use this base

---
+++ src/UrlShortningEvent.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of PhergieUrl.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WyriHaximus\Phergie\Plugin\Url;
+
+/**
+ *
+ *
+ * @category Phergie
+ * @package WyriHaximus\Phergie\Plugin\Url
+ */
+class UrlShortningEvent
+{
+}
