77918b36160496129768fccaf3a10ab6f7431a93
Cees-Jan <ceesjank@gmail.com>
2014-08-10 12:11:15 +0200

    Test getting and setting of the URL handler

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -10,6 +10,7 @@
 
 namespace WyriHaximus\Phergie\Tests\Plugin\Url;
 
+use Phake;
 use WyriHaximus\Phergie\Plugin\Url\Plugin;
 
 /**
@@ -48,5 +49,28 @@ class PluginTest extends \PHPUnit_Framework_TestCase
         $plugin = new Plugin();
         $plugin->setLogger($logger);
         $plugin->logDebug('foo:bar');
+    }
+
+    public function testGetHandler() {
+        $plugin = new Plugin();
+        $this->assertTrue(in_array('WyriHaximus\Phergie\Plugin\Url\UrlHandlerInterface', class_implements($plugin->getHandler())));
+        $this->assertInstanceOf('\WyriHaximus\Phergie\Plugin\Url\DefaultUrlHandler', $plugin->getHandler());
+    }
+
+    public function testCustomHandler() {
+        $handler = Phake::mock('\WyriHaximus\Phergie\Plugin\Url\DefaultUrlHandler');
+        $plugin = new Plugin(array(
+            'handler' => $handler,
+        ));
+        $this->assertTrue(in_array('WyriHaximus\Phergie\Plugin\Url\UrlHandlerInterface', class_implements($plugin->getHandler())));
+        $this->assertSame($handler, $plugin->getHandler());
+    }
+
+    public function testStdClassHandler() {
+        $handler = new \stdClass();
+        $plugin = new Plugin(array(
+            'handler' => $handler,
+        ));
+        $this->assertInstanceOf('\WyriHaximus\Phergie\Plugin\Url\DefaultUrlHandler', $plugin->getHandler());
     }
 }
