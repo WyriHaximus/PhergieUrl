f592e560bdb7869b5a15066da8bafd4613f9212a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-08-17 21:38:59 +0200

    Removed dead arguments

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -250,14 +250,15 @@ class PluginTest extends \PHPUnit_Framework_TestCase
             'foo' => 'bar',
         ), 200);
 
-        Phake::when($plugin)->emitShortningEvents($this->isType('string'), $this->isType('string'), $event, $queue)->thenReturn(Phake::mock('React\Promise\PromiseInterface'));
+        Phake::when($plugin)->emitShortningEvents($this->isType('string'), $this->isType('string'))->thenReturn(Phake::mock('React\Promise\PromiseInterface'));
+
         $request->callResolve('', array(
             'foo' => 'bar',
         ), 200);
 
         Phake::inOrder(
             Phake::verify($plugin, Phake::times(2))->logDebug($this->isType('string')),
-            Phake::verify($plugin)->emitShortningEvents($this->isType('string'), $this->isType('string'), $event, $queue)
+            Phake::verify($plugin)->emitShortningEvents($this->isType('string'), $this->isType('string'))
         );
     }
 
